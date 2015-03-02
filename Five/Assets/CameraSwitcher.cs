93067c8217a0d00a99fb3c7f78606c2fe7183a01
Hungrymeanie <zack.zepkin@gmail.com>
2015-03-01 16:07:01 -0800

    adding monitor UI

---
+++ Five/Assets/CameraSwitcher.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitcher : MonoBehaviour {
+	public GameObject Closet;
+	public GameObject Backstage;
+	public GameObject Dining;
+	public GameObject East;
+	public GameObject EastC;
+	public GameObject Pirate;
+	public GameObject Restrooms;
+	public GameObject Show;
+	public GameObject West;
+	public GameObject WestC;
+	public GameObject Main;
+	// Use this for initialization
+	void Start () {
+		Main = Camera.main.gameObject;
+	}
+	public void Closet_Cam (){
+		}
+	public void Backstage_Cam (){
+		}
+	public void Dining_Cam () {
+		}
+	public void East_Cam () {
+		}
+	public void EastC_Cam () {
+		}
+	public void Pirate_Cam () {
+		}
+	public void Restrooms_Cam () {
+		}
+	public void Show_Cam () {
+		}
+	public void West_Cam () {
+		}
+	public void WestC_Cam () {
+		}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Five/Assets/Toggle_Monitor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Toggle_Monitor : MonoBehaviour {
+	public bool toggleMonitor = false;
+	public bool previous = false;
+	public GameObject Monitor; 
+	// Use this for initialization
+	void Start () {
+		Monitor.SetActive (false);
+	}
+	public void toggle () {
+		toggleMonitor = !toggleMonitor;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (toggleMonitor != previous) {
+			if (toggleMonitor == true) {
+				Monitor.SetActive (true);
+			}else{
+				Monitor.SetActive (false);
+			}
+		}
+previous = toggleMonitor;
+	}
+}
